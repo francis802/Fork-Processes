5f81e74cbc7292b0975c4f2623145b9c17b7bf89
João Silva <up202108726@up.pt>
2022-11-14 17:12:49 +0000

    Cleaned the code

--- Q1.c
+++ Q1.c
@@ -2,42 +2,39 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-
-    /* check if exactly one argument is present */
+    // Check if the user has entered the correct number of arguments
     if (argc < 4) {
         printf("correct usage: samples filename n_positions m_characters\n");
         return EXIT_FAILURE;
     }
 
-    int n, m;
-
     FILE* file;
     file = fopen(argv[1],"r");
 
+    // Check if the file exists
     if (file == NULL) {
         printf("error: cannot open %s\n", argv[1]);
         return EXIT_FAILURE;
     }
 
-    n = atoi(argv[2]);
-    m = atoi(argv[3]);
+    int n = atoi(argv[2]);
+    int m = atoi(argv[3]);
 
-
+    //Counts the number of characters in the file
     int count = 0;
     char ch;
-    /* Count the amount of characters in the file */
     for (ch = getc(file); ch != EOF; ch = getc(file))
-        // Increment count for this character
         count = count + 1;
 
+    //This substraction is to account for the possibility of the text section being in the last m positions of the file
     count-=m;
-
-
+    
     srand(0);
 
+    //Prints the random text sections of length m from the file
     int random;
     int j; char c;
-    for(int i=0; i<=n; i++) {
+    for(int i=0; i<n; i++) {
         j = 0;
         random = rand();
         random%=count;
