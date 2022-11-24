92035732f9f658689fa47c61ded36902d93fe859
Francisco Campos <franciscosccampos@gmail.com>
2022-11-24 01:39:59 +0000

    Q2 100% complete

--- Q2.c
+++ Q2.c
@@ -8,21 +8,27 @@
 int main(int argc, char* argv[]){
     int i;
     pid_t pid;
+
+    //Create array to store all commands needed to zip the ebooks
     char*  lst_files[argc+1];
     lst_files[0] = "zip";
     lst_files[1] = "ebooks.zip";
+
     char* file_name_with_epub;
+
     for(i=1; i<argc; i++){
+        //Substitue the .txt extension in file for .epub and save it in array
         file_name_with_epub = strcat(strncpy(malloc(strlen(argv[i])+5), argv[i], strlen(argv[i])-4), ".epub");
         lst_files[i+1] = file_name_with_epub;
+
         pid = fork();
-        if(pid == 0){
-            printf("Child %d: %s, %s\n", i, file_name_with_epub, argv[i]);
+        if (pid != 0) printf("[pid%d] converting %s ...\n", pid, argv[i]);
+        else {
             execlp("pandoc", "pandoc", argv[i], "-o", file_name_with_epub, NULL);
         }
-        wait(&pid);
+        wait(&pid); //Wait for child procedure to die
     }
-    lst_files[argc+1] = NULL;
+    lst_files[argc+1] = NULL; //Last element of array must be NULL for execvp to operate well
     execvp("zip", lst_files);
     return 0;
 }
