fe98a8b9ab9ecce1f6ba63773c6010e67ca70e3d
Francisco Campos <franciscosccampos@gmail.com>
2023-01-23 20:46:09 +0000

    Bug Fix Q2.c

--- Q2/Q2.c
+++ Q2/Q2.c
@@ -13,10 +13,10 @@ int main(int argc, char* argv[]){
     }
 
     int i;
-    pid_t pid;
+    pid_t pid_array[argc-1];
 
     //Create array to store all commands needed to zip the ebooks
-    char*  lst_files[argc+1];
+    char*  lst_files[argc+2];
     lst_files[0] = "zip";
     lst_files[1] = "ebooks.zip";
 
@@ -27,20 +27,25 @@ int main(int argc, char* argv[]){
         file_name_with_epub = strcat(strncpy(malloc(strlen(argv[i])+5), argv[i], strlen(argv[i])-4), ".epub");
         lst_files[i+1] = file_name_with_epub;
 
-        pid = fork();
-        if (pid < 0) {
+        pid_array[i-1] = fork();
+        if (pid_array[i-1] < 0) {
             //Error forking
             perror("fork failed");
             return EXIT_FAILURE;
         }
 
-        if (pid != 0) printf("[pid%d] converting %s ...\n", pid, argv[i]);
+        if (pid_array[i-1] != 0) printf("[pid%d] converting %s ...\n", pid_array[i-1], argv[i]);
         else {
             execlp("pandoc", "pandoc", argv[i], "-o", file_name_with_epub, NULL);
         }
-        waitpid(pid, NULL, 0);//Wait for child procedure to die
     }
     lst_files[argc+1] = NULL; //Last element of array must be NULL for execvp to operate well
+    for(int i = 0; i < argc -1; i++) { //Multi-process
+        if(waitpid(pid_array[i], NULL, 0) < 0) {
+            perror("waitpid");
+            return EXIT_FAILURE;
+        }
+    }
     execvp("zip", lst_files);
     return 0;
 }
